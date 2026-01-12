8cf1393563f33f91dcc53a2cf78558c313a88dd7
Ali Gunawan <ali.gunawann125@gmail.com>
2026-01-12 12:02:32 +0700

    Add files via upload

---
+++ project_data_mahasiswa/app/views/user/navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+  <div class="container">
+    <a class="navbar-brand" href="?url=user/students">Data Mahasiswa</a>
+
+    <div class="navbar-nav">
+      <a class="nav-link" href="?url=user/students">Mahasiswa</a>
+      <a class="nav-link text-warning" href="?url=logout">Logout</a>
+    </div>
+  </div>
+</nav>
